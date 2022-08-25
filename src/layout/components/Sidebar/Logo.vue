b4663efcd140db8f2129fddc9555244bb466f34a
wancihua <wancihua@youcash.com>
2022-08-25 19:22:24 +0800

    feat: 优化

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -28,7 +28,7 @@ export default {
   },
   data() {
     return {
-      title: '创尊' + (isTestApi ? '-测试' : ''),
+      title: '创尊优品',
       logo: ''
       // logo: 'https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png'
     }
